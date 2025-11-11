3363f4a529e245b5fef12ce8f2da858e6e9ed236
Tikson Tom <tikson+frgr@qburst.com>
2025-11-12 00:10:46 +0530

    initial commit

--- app/src/main/java/com/composeappliction/MainActivity.kt
+++ app/src/main/java/com/composeappliction/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.composeappliction.ui.theme.ComposeApplictionTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
